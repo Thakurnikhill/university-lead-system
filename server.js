8b875fb0f844a0f8a1e2eb60cd28e30e84541541
Thakurnikhill <nikhilthakur9835@gmail.com>
2025-11-14 15:15:48 +0530

    Fix Vercel routes & public folder

--- server.js
+++ server.js
@@ -9,9 +9,8 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.static('public'));
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/public/index.html');
-});
+app.get('/', (req, res) => res.sendFile(__dirname + '/public/index.html'));
+
 
 // University Data
 const universities = {
